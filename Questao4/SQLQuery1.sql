f8d56140ce3cb4e10e2d80c55faed6cac46260ec
rafaelcarlins <rafaelcarlins@gmail.com>
2024-04-22 15:29:23 -0300

    questão 4

---
+++ Questao4/SQLQuery1.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE atendimentos (
+    id  UNIQUEIDENTIFIER DEFAULT NEWID() NOT NULL,
+    assunto VARCHAR(100) NOT NULL,
+    ano INT
+);
+
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao atendimento','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao produto','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao produto','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao atendimento','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao produto','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao produto','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao produto','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao produto','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao atendimento','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao atendimento','2021');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao produto','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao produto','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao atendimento','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao atendimento','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao atendimento','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2022');
+INSERT INTO atendimentos (assunto, ano) VALUES ('Reclamacao cadastro','2022');
+COMMIT;
+
+
+SELECT assunto, ano, count(ano) as QtdeOcorrencias FROM atendimentos
+GROUP BY assunto, ano
+HAVING COUNT(ano) > 3;
